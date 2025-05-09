316edae207b7c4cf49fd35630676027cc677853f
matt-newhall <matt@newhall.dev>
2025-05-09 03:10:39 +0100

    fix: move damage not calculating with stab and type effectiveness

--- src/battle/battle_lib.c
+++ src/battle/battle_lib.c
@@ -7998,6 +7998,8 @@ int BattleAI_PostKOSwitchIn(BattleSystem *battleSys, int battler)
             isTrainerKOAI = 0;
             score = 0;
 
+            BattleSystem_InitBattleMon(battleSys, battleCtx, battler, i);
+
             for (j = 0; j < LEARNED_MOVES_MAX; j++) {
                 moveDefender = Pokemon_GetValue(defenderPokemon, MON_DATA_MOVE1 + j, NULL);
 
@@ -8012,6 +8014,15 @@ int BattleAI_PostKOSwitchIn(BattleSystem *battleSys, int battler)
                         defender,
                         battler,
                         1);
+
+                    damageToTarget = BattleSystem_ApplyTypeChart(battleSys,
+                        battleCtx,
+                        moveDefender,
+                        NULL,
+                        defender,
+                        battler,
+                        damageToTarget,
+                        &battleCtx->moveStatusFlags);
 
                     if (damageToTarget >= battlerPokemonCurHP) {
                         // AI can be OHKO'd by the player
@@ -8050,6 +8061,15 @@ int BattleAI_PostKOSwitchIn(BattleSystem *battleSys, int battler)
                         battler,
                         defender,
                         1);
+
+                    damageToTarget = BattleSystem_ApplyTypeChart(battleSys,
+                        battleCtx,
+                        moveBattler,
+                        NULL,
+                        battler,
+                        defender,
+                        damageToTarget,
+                        &battleCtx->moveStatusFlags);
 
                     if (damageToTarget >= defenderPokemonCurHP) {
                         if (moveBattler == MOVE_PURSUIT) {
