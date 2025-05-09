7627f471350316985eb1094b83abe0cbc60e385a
matt-newhall <matt@newhall.dev>
2025-05-09 03:48:07 +0100

    feat: prioritise trapping ko's

--- src/battle/battle_lib.c
+++ src/battle/battle_lib.c
@@ -8096,9 +8096,21 @@ int BattleAI_PostKOSwitchIn(BattleSystem *battleSys, int battler)
             }
 
             if (isAIKOTrainer && (battlerPokemonSpeed >= defenderPokemonSpeed)) {
+                if (Battler_IsTrapped(battleSys, battleCtx, defender)) {
+                    return i;
+                }
                 if (isPursuitKO) {
                     return i;
                 }
+                score = 7;
+                if (score > maxScore) {
+                    maxScore = score;
+                    picked = i;
+                }
+                continue;
+            }
+
+            if (isAIKOTrainer && isPursuitKO) {
                 score = 6;
                 if (score > maxScore) {
                     maxScore = score;
