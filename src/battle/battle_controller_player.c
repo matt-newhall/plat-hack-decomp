5d0907340e3b09f723eb8f4b7ef9726a586f0f60
matt-newhall <matt@newhall.dev>
2025-06-09 01:28:08 +0100

    [ABILITY] Keen Eye ignores target's evasion

--- src/battle/battle_controller_player.c
+++ src/battle/battle_controller_player.c
@@ -2898,7 +2898,7 @@ static int BattleControllerPlayer_CheckMoveHitAccuracy(BattleSystem *battleSys, 
     if (Battler_IgnorableAbility(battleCtx, attacker, defender, ABILITY_UNAWARE) == TRUE) {
         accStages = 0;
     }
-    if (Battler_Ability(battleCtx, attacker) == ABILITY_UNAWARE) {
+    if (Battler_Ability(battleCtx, attacker) == ABILITY_UNAWARE || Battler_Ability(battleCtx, attacker) == ABILITY_KEEN_EYE) {
         evaStages = 0;
     }
     if (MON_IS_IDENTIFIED(defender) && evaStages < 0) {
