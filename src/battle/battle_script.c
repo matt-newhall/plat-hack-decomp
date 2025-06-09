da45e6b826342ec29948c944bc3090b0d3cb0489
matt-newhall <matt@newhall.dev>
2025-06-09 02:42:14 +0100

    [MOVE] Triple Kick + Skill Link has only one accuracy check

--- src/battle/battle_script.c
+++ src/battle/battle_script.c
@@ -2883,6 +2883,11 @@ static BOOL BtlCmd_SetMultiHit(BattleSystem *battleSys, BattleContext *battleCtx
         battleCtx->multiHitAccuracyCheck = flags;
     }
 
+    if (Battler_Ability(battleCtx, battleCtx->attacker) == ABILITY_SKILL_LINK) {
+        flags |= SYSCTL_SKIP_ACCURACY_CHECK;
+        battleCtx->multiHitAccuracyCheck = flags;
+    }
+
     return FALSE;
 }
 
