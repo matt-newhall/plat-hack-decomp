11647ce9246319ab7cb63a414df004823d5ad191
matt-newhall <matt@newhall.dev>
2025-06-10 01:07:15 +0100

    [MOVE] Spit Up now does variable damage

--- res/battle/scripts/effects/effect_script_0161.s
+++ res/battle/scripts/effects/effect_script_0161.s
@@ -17,7 +17,7 @@ _000:
     BufferMessage BattleStrings_Text_PokemonsStockpiledEffectWoreOff_Ally, TAG_NICKNAME, BTLSCR_ATTACKER
     UpdateVar OPCODE_SET, BTLVAR_SIDE_EFFECT_FLAGS_INDIRECT, MOVE_SIDE_EFFECT_ON_HIT|MOVE_SUBSCRIPT_PTR_PRINT_MESSAGE_AND_PLAY_ANIMATION
     CalcCrit 
-    CalcMaxDamage 
+    CalcDamage 
     End 
 
 _064:
