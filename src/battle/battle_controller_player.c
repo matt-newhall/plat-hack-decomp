fa0b353531a0488f9e37b1b20d6a271388a599fc
matt-newhall <matt@newhall.dev>
2025-07-27 22:28:24 +0100

    [ABILITY] Illuminate copies Keen Eye effect

--- src/battle/battle_controller_player.c
+++ src/battle/battle_controller_player.c
@@ -2920,7 +2920,9 @@ static int BattleControllerPlayer_CheckMoveHitAccuracy(BattleSystem *battleSys, 
     if (Battler_IgnorableAbility(battleCtx, attacker, defender, ABILITY_UNAWARE) == TRUE) {
         accStages = 0;
     }
-    if (Battler_Ability(battleCtx, attacker) == ABILITY_UNAWARE || Battler_Ability(battleCtx, attacker) == ABILITY_KEEN_EYE) {
+    if (Battler_Ability(battleCtx, attacker) == ABILITY_UNAWARE
+        || Battler_Ability(battleCtx, attacker) == ABILITY_KEEN_EYE
+        || Battler_Ability(battleCtx, attacker) == ABILITY_ILLUMINATE) {
         evaStages = 0;
     }
     if (MON_IS_IDENTIFIED(defender) && evaStages < 0) {
