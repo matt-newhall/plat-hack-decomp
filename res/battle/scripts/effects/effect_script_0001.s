b5a1450572a4e4bcee4326873353d7ba32de776f
matt-newhall <matt@newhall.dev>
2025-08-11 01:29:14 +0100

    [MOVE] Safeguard now protects the user from status-inducing abilities (e.g. Effect Spore), but no longer blocks berry confusion (via any means), or Flame/Toxic Orb from inflicting status

--- res/battle/scripts/effects/effect_script_0001.s
+++ res/battle/scripts/effects/effect_script_0001.s
@@ -3,5 +3,20 @@
     .data
 
 _000:
+    CompareVarToValue OPCODE_EQU, BTLVAR_CURRENT_MOVE, MOVE_DARK_VOID, _001
+    GoTo _002
+
+_001:
+    CompareMonDataToValue OPCODE_EQU, BTLSCR_ATTACKER, BATTLEMON_SPECIES, 491, _002
+    PrintAttackMessage 
+    Wait 
+    WaitButtonABTime 30
+    PrintMessage pl_msg_00000368_00796, TAG_NONE
+    Wait 
+    WaitButtonABTime 30
+    UpdateVar OPCODE_FLAG_ON, BTLVAR_MOVE_STATUS_FLAGS, MOVE_STATUS_NO_MORE_WORK
+    End
+
+_002:
     UpdateVar OPCODE_SET, BTLVAR_SIDE_EFFECT_FLAGS_DIRECT, MOVE_SIDE_EFFECT_TO_DEFENDER|MOVE_SUBSCRIPT_PTR_SLEEP
-    End 
+    End
