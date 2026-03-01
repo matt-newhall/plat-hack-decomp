234030b79afcf1537299cc26bebe2284afccbb66
matt-newhall <matt@newhall.dev>
2026-03-01 18:55:11 +0000

    [ABILITY] Add Telepathy

--- src/battle/battle_controller.c
+++ src/battle/battle_controller.c
@@ -3041,6 +3041,16 @@ static int BattleController_CheckMoveHitOverrides(BattleSystem *battleSys, Battl
         return 0;
     }
 
+    int battleType = BattleSystem_BattleType(battleSys);
+
+    if ((battleType & BATTLE_TYPE_DOUBLES)
+        && Battler_IgnorableAbility(battleCtx, battleCtx->attacker, battleCtx->defender, ABILITY_TELEPATHY)
+        && Battler_Side(battleSys, battleCtx->attacker) == Battler_Side(battleSys, battleCtx->defender)
+        && (CURRENT_MOVE_DATA.class == CLASS_PHYSICAL || CURRENT_MOVE_DATA.class == CLASS_SPECIAL)) {
+        battleCtx->moveStatusFlags |= MOVE_STATUS_TELEPATHY;
+        return 0;
+    }
+
     if (battleCtx->turnFlags[defender].protecting
         && (MOVE_DATA(move).flags & MOVE_FLAG_CAN_PROTECT)
         && (move != MOVE_CURSE || Move_IsGhostCurse(battleCtx, move, attacker) == TRUE) // Ghost-Curse can be Protected
