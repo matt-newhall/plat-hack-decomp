21fe7a31e09c0cccd6f7abf955d5b38881fecad8
Viperio <146665252+Viperio19@users.noreply.github.com>
2026-04-02 05:12:22 +0200

    Document route 218, canalave city and full-/newmoon island scripts (#1049)

--- include/unk_0205DFC4.h
+++ include/unk_0205DFC4.h
@@ -15,7 +15,7 @@ u16 Pokedex_GetRatingMessageID_National(u16 pokemonCaught, u16 playerGender);
 u16 SaveData_GetFirstNonEggInParty(SaveData *saveData);
 BOOL HasAllLegendaryTitansInParty(SaveData *saveData);
 void sub_0205E318(FieldTask *param0, MapObject *param1, u16 param2, u16 param3, u16 param4, u16 param5);
-void sub_0205E3F4(FieldTask *param0, MapObject *param1, u16 param2, u16 param3);
+void MapObject_Flicker(FieldTask *task, MapObject *mapObj, u16 times, u16 delay);
 enum BattleFrontierStatsIndex BattleFrontierStats_GetFactoryLatestStreakIdx(u8 isOpenLevel, u8 challengeType);
 enum BattleFrontierStatsIndex BattleFrontierStats_GetFactoryRecordStreakIdx(u8 isOpenLevel, u8 challengeType);
 enum BattleFrontierStatsIndex BattleFrontierStats_GetFactoryLatestTradeCountIndex(u8 isOpenLevel, u8 challengeType);
@@ -31,11 +31,11 @@ enum BattleFrontierStatsIndex BattleFrontierStats_GetCastleRecordStreakIndex(u8 
 enum BattleFrontierStatsIndex BattleFrontierStats_GetCastleLatestCPIndex(u8 challengeType);
 enum BattleFrontierStatsIndex BattleFrontierStats_GetCastleSpentCPIndex(u8 challengeType);
 enum BattleFrontierStatsIndex BattleFrontierStats_GetCastleRecordCPIndex(u8 challengeType);
-int BattleFrontierStats_GetHostFriendIdx(u32 statIndex);
-u8 sub_0205E6D8(SaveData *saveData);
 enum BattleFrontierStatsIndex BattleFrontierStats_GetArcadeLatestStreakIndex(u8 challengeType);
 enum BattleFrontierStatsIndex BattleFrontierStats_GetArcadeCurrentStreakIndex(u8 challengeType);
 enum BattleFrontierStatsIndex BattleFrontierStats_GetTowerLatestStreakIndex(u8 challengeType);
 enum BattleFrontierStatsIndex BattleFrontierStats_GetTowerRecordStreakIndex(u8 challengeType);
+int BattleFrontierStats_GetHostFriendIdx(u32 statIndex);
+u8 sub_0205E6D8(SaveData *saveData);
 
 #endif // POKEPLATINUM_UNK_0205DFC4_H
