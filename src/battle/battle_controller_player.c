0fffa14ca64c890a11c2a00139f58db8b31d2582
matt-newhall <matt@newhall.dev>
2026-04-04 20:08:27 +0100

    [NEW MOVE] Add Bulldoze

--- src/battle/battle_controller_player.c
+++ src/battle/battle_controller_player.c
@@ -4837,7 +4837,7 @@ static void BattleControllerPlayer_ClearFlags(BattleSystem *battleSys, BattleCon
  * @param onlyFaint         If TRUE, then standard FAINT_MON subroutine will be
  *                          loaded if any battler has fainted. If FALSE, then the
  *                          subroutine which checks for on-faint triggers will be
- *                          loaded instead (e.g., Destiny Bond, Grudge).
+ *                          loaded instead (e.g., Destiny Bond).
  * @return TRUE if any battler has fainted; FALSE if none have fainted.
  */
 static BOOL BattleControllerPlayer_AnyFainted(BattleContext *battleCtx, int nextCmd, int nextCmdNoFainted, BOOL onlyFaint)
