f3a6a5ba5d5a98febfa6c223e6a61e5fe3269ecb
matt-newhall <matt@newhall.dev>
2026-04-25 02:50:08 +0100

    feat: Nature Power should be single target, not used on user

--- res/battle/scripts/effects/effect_script_0173.s
+++ res/battle/scripts/effects/effect_script_0173.s
@@ -11,4 +11,4 @@ _000:
     PrintMessage BattleStrings_Text_MoveTurnedIntoMove, TAG_MOVE_MOVE, BTLSCR_ATTACKER, BTLSCR_MSG_TEMP
     Wait 
     WaitButtonABTime 30
-    GoToMoveScript FALSE
+    GoToMoveScript TRUE
