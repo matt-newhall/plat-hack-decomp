00f0ca58ff3c62d1c337a5a6721e958b6e13c4fb
matt-newhall <matt@newhall.dev>
2026-04-29 03:37:51 +0100

    fix: power-up punch, embargo and trump card discrepancy

--- res/battle/moves/power_up_punch/anim.s
+++ res/battle/moves/power_up_punch/anim.s
@@ -1,22 +1,13 @@
 #include "macros/btlanimcmd.inc"
 
 L_0:
-    LoadParticleResource 0, trump_card_spa
-    PlaySoundEffectR SEQ_SE_DP_W376
+    LoadParticleResource 0, embargo_spa
+    PlaySoundEffectR SEQ_SE_DP_W373
     CreateEmitter 0, 0, EMITTER_CB_GENERIC
     SetExtraParams 0, 2, 2, 0, 0, 0
     CreateEmitter 0, 1, EMITTER_CB_GENERIC
     SetExtraParams 0, 2, 2, 0, 0, 0
-    CreateEmitter 0, 2, EMITTER_CB_GENERIC
-    SetExtraParams 0, 2, 2, 0, 0, 0
-    CreateEmitter 0, 3, EMITTER_CB_GENERIC
-    SetExtraParams 0, 2, 2, 0, 0, 0
-    Delay 43
-    PlaySoundEffectR SEQ_SE_DP_W013
-    Delay 3
-    PlaySoundEffectR SEQ_SE_DP_W376B
-    Delay 4
-    Func_Shake 2, 0, 1, 2, BATTLE_ANIM_BATTLER_SPRITE_DEFENDER
+    Func_FadeBattlerSprite BATTLE_ANIM_DEFENDER, 0, 1, BATTLE_COLOR_BLACK, 10, 20
     WaitForAllEmitters
     UnloadParticleSystem 0
     End
