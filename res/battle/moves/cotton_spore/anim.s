ea3f6b3b90bad3f2922d3a7e0a987fd70a534935
matt-newhall <matt@newhall.dev>
2026-05-03 01:23:23 +0100

    fix: Cotton Spore animation targets two in Double battles

--- res/battle/moves/cotton_spore/anim.s
+++ res/battle/moves/cotton_spore/anim.s
@@ -2,8 +2,9 @@
 
 L_0:
     LoadParticleResource 0, cotton_spore_spa
-    CreateEmitter 0, 0, EMITTER_CB_SET_POS_TO_DEFENDER
+    CreateEmitter 0, 0, EMITTER_CB_SET_POS_TO_DEFENDER_SIDE
     Func_FadeBattlerSprite BATTLE_ANIM_DEFENDER, 0, 3, BATTLE_COLOR_WHITE, 10, 10
+    Func_FadeBattlerSprite BATTLE_ANIM_DEFENDER_PARTNER, 0, 3, BATTLE_COLOR_WHITE, 10, 10
     PlayLoopedSoundEffectR SEQ_SE_DP_W199, 3, 8
     WaitForAllEmitters
     UnloadParticleSystem 0
