d76d2c8f1942206f729a70898aece9a562bc3d15
matt-newhall <matt@newhall.dev>
2026-05-30 10:03:36 +0100

    fix: Parting Shot is a sound-based move

--- src/battle/battle_lib.c
+++ src/battle/battle_lib.c
@@ -3805,7 +3805,8 @@ static u16 sSoundMoves[] = {
     MOVE_HEAL_BELL,
     MOVE_SNARL,
     MOVE_BOOMBURST,
-    MOVE_DISARMING_VOICE
+    MOVE_DISARMING_VOICE,
+    MOVE_PARTING_SHOT
 };
 
 BOOL BattleSystem_IsSoundMove(u16 move) {
