068a4519a646136cdb5b526858678d9dd321135a
matt-newhall <matt@newhall.dev>
2026-09-06 21:46:14 +0100

    feat: remove odd keystone on route 208

--- res/field/scripts/scripts_route_208.s
+++ res/field/scripts/scripts_route_208.s
@@ -29,26 +29,8 @@ Route208_BlackBelt:
     PlaySE SEQ_SE_CONFIRM
     LockAll
     FacePlayer
-    GoToIfSet FLAG_RECEIVED_ROUTE_208_ODD_KEYSTONE, Route208_GoToRoute209
-    Message Route208_Text_JustTakeIt
-    SetVar VAR_0x8004, ITEM_ODD_KEYSTONE
-    SetVar VAR_0x8005, 1
-    GoToIfCannotFitItem VAR_0x8004, VAR_0x8005, VAR_RESULT, Route208_BagIsFull
-    SetFlag FLAG_RECEIVED_ROUTE_208_ODD_KEYSTONE
-    Common_GiveItemQuantityNoLineFeed
-    CloseMessage
-    ReleaseAll
-    End
-
-Route208_GoToRoute209:
     Message Route208_Text_GoToRoute209
     WaitButton
-    CloseMessage
-    ReleaseAll
-    End
-
-Route208_BagIsFull:
-    Common_MessageBagIsFull
     CloseMessage
     ReleaseAll
     End
